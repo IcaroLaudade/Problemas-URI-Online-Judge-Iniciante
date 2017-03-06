d112014a1b4b60d5af4783e1d0275c242cc8bdeb
icaro alcantara laudade <icaroalcantara11@hotmail.com>
2017-03-06 16:12:29 -0300

    Problema 1004 - Produto simples -Python
    Problema 1004 - Produto simples -Python

---
+++ 1004/src/1004.py
@@ -0,0 +1,10 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    a=int(raw_input(""))
+    b=int(raw_input(""))
+    prod=a*b
+    print "PROD = %s" % prod
+    
