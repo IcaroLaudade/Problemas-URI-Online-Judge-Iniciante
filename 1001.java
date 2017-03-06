dc450f32c108fd2fc247586f2c68c81388683aed
icaro alcantara laudade <icaroalcantara11@hotmail.com>
2017-03-06 13:48:47 -0300

    Problema 1001 - Extremamente básico -Java
    Problema 1001 - Extremamente básico -Java

---
+++ 1001.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+
+import java.util.Scanner;
+ 
+
+   
+public class Main 
+	{
+ 
+    
+	public static void main(String[] args) throws IOException 
+		{
+ 
+    
+		Scanner s = new Scanner(System.in);
+        
+   
+ 		int a=s.nextInt();
+   
+		int b=s.nextInt();
+    
+		int x =a+b;
+    
+		System.out.println("X = "+x);
+ 
+    
+		}
+ 
+
+	}
