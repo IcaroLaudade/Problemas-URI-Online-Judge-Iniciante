3841811c64234854cbcf8f053e0a695f6377c527
icaro alcantara laudade <icaroalcantara11@hotmail.com>
2017-03-06 13:43:07 -0300

    Problema 1003 - Soma simples -Python
    Problema 1003 - Soma simples -Python

---
+++ 1003/src/1003.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    a=int(raw_input(''))
+    b=int(raw_input(''))
+    soma=a+b
+    print "SOMA = %s" % soma
