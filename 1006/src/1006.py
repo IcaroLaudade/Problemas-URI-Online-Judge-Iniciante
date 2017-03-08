6dbb7f7781b368f666b46a01833973711b08021e
Icaro <Icaro@Icaro-PC>
2017-03-08 14:11:54 -0300

    Problema 1006 - Média 2 -Python

---
+++ 1006/src/1006.py
@@ -0,0 +1,11 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    a=float(raw_input())
+    b=float(raw_input())
+    c=float(raw_input())
+    media = ((a*2)+(b*3)+(c*5)) / 10
+    print "MEDIA = %.1f" %media
+   
