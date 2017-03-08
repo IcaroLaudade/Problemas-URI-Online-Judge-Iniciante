b79e6b6a0f45ac3aea0067b6a0fbd85a31b2ae45
Icaro <Icaro@Icaro-PC>
2017-03-08 13:55:00 -0300

    Problema 1005 - Média 1 -Python

---
+++ 1005/src/1005.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    a=float(raw_input())
+    b=float(raw_input())
+    media = ((a*3.5)+(b*7.5)) / 11
+    print "MEDIA = %.5f" %media
