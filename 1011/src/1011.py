a8cd11e9c309670a33f07d35787ba5ea143ddfc0
Icaro <Icaro@Icaro-PC>
2017-03-10 15:16:00 -0300

    Problema 1011 - Esfera -Python

---
+++ 1011/src/1011.py
@@ -0,0 +1,8 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    raio=float(raw_input(""))
+    volume=(4/3.0)*3.14159*(raio**3)
+    print "VOLUME = %.3f" % volume
