d647967ddc9e836ccd06057c922870dea6d94c8d
Icaro <Icaro@Icaro-PC>
2017-03-14 13:08:32 -0300

    Problema 1096- Sequencia IJ 2 -Python

---
+++ 1096/src/1096.py
@@ -0,0 +1,14 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    i=1
+    while True:
+        print "I=%s"% i +" J=7"
+        print "I=%s"% i +" J=6"
+        print "I=%s"% i +" J=5"
+        i+=2
+        if i>9:
+            break
+
