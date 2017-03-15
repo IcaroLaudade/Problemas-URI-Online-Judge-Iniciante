f7fd10ad06f6de6a6ee842e141cb2e03675f88cf
Icaro <Icaro@Icaro-PC>
2017-03-15 18:03:56 -0300

    Problema 1153- Fatorial Simples -Python

---
+++ 1153/src/1153.py
@@ -0,0 +1,7 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+import math
+if __name__ == "__main__":
+    a=int(raw_input())
+    print math.factorial(a)
