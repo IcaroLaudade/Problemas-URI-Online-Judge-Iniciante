e40dfebc79803787c700dbfeab4c174956f5afa2
Icaro <Icaro@Icaro-PC>
2017-03-15 21:50:20 -0300

    Problema 1060 - Números Positivos -Java

--- 1060/src/p1060/Problema1060.java
+++ 1060/src/p1060/Problema1060.java
@@ -23,6 +23,7 @@ public class Problema1060 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException{
+        //Buffer     
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int contadorPositivo=0;
         for (int contador=0;contador<6;contador++){
