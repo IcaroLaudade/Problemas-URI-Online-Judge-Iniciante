cf391c661d36d4234ce4dcf99766d1fa99b4afbf
Icaro <Icaro@Icaro-PC>
2017-03-16 13:42:03 -0300

    Problema 1014 - Consumo -Python

---
+++ 1014Python/src/1014.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+if __name__ == "__main__":
+    x=int(raw_input())
+    y=float(raw_input())
+    print "%.3f km/l" %(x/y)
+
