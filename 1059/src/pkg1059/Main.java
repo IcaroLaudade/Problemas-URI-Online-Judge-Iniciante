15c7f58f5e52c97a88179b1f4927275d7e0e443d
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-04-15 21:35:47 -0300

    Problema 1059 - Números Pares -Java

---
+++ 1059/src/pkg1059/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1059;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (short i = 2; i < 101; i = (short) (i + 2)) {
+            System.out.println(i);
+        }
+    }
+
+}
