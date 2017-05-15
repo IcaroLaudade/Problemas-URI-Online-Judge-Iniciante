d942e2bea578c7b60db7c8a7ddc713cf6b2d9229
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-05-15 17:14:47 -0300

    Problema 1114-Senha Fixa-Python

---
+++ 1114.py
@@ -0,0 +1,9 @@
+if __name__=="__main__":    
+    while True:
+        a=int(input())
+        if a!=2002:
+            print ("Senha Invalida")
+        else:
+            print ("Acesso Permitido")
+            break
+        
