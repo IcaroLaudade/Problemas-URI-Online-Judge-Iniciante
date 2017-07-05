fe376fe94007d28080cf4ff45955254aa3b5f545
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-05 19:47:19 -0300

    Problema 2167-Falha do Motor-C#

---
+++ 2167/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerosRotacao = int.Parse(Console.ReadLine());
+            String[] numeroes = Console.ReadLine().Split(' ');
+            int[] array = Array.ConvertAll(numeroes, int.Parse);
+            for (int i = 1; i < array.Length;i++ )
+            {
+                if (array[i - 1] > array[i]) {
+                    Console.WriteLine(i+1);
+                    break;
+                }
+                if (i == array.Length - 1) {
+                    Console.WriteLine(0);
+                    break;
+                }
+            }
+        }
+    }
+}
