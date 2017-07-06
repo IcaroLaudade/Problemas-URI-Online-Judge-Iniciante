69f8618b22351aa1947b85c62cd3c7a6d0471fcb
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-06 19:46:19 -0300

    Problema 2061-As Abas de Péricles-C#

---
+++ 2061/2061/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2061
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros=Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+            int total = numeros[0];
+            for (int i = 0; i < numeros[1];i++ )
+            {
+                String texto = Console.ReadLine();
+                if (texto.Equals("fechou"))
+                {
+                    total++;
+                }
+                else if (texto.Equals("clicou"))
+                {
+                    total--;
+                }
+
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
