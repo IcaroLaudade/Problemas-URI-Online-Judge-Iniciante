fef0b55682cff4b038b37352c2989de13d6cc1f2
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-06 15:57:29 -0300

    Problema 2160-Nome no Formulário-C#

---
+++ 2160/2160/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Console.ReadLine().Length;
+            if (t < 81)
+            {
+                Console.WriteLine("YES");
+            }
+            else {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
