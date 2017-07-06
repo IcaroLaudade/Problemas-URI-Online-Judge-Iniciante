f554c3bdbf4faefbb696b8f524e322d2395f2608
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-06 20:28:55 -0300

    Problema 2152-PEPE, JÁ TIREI A VELA!-C#

---
+++ 2152/2152/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n;i++ )
+            {
+                int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                if (array[2] == 0 && array[0] < 10 && array[1]<10)
+                {
+                    Console.WriteLine("0"+array[0] + ":0" + array[1] + " - A porta fechou!");
+                }
+                else if (array[2] == 1 && array[0] < 10 && array[1] <10)
+                {
+                    Console.WriteLine("0"+array[0] + ":0" + array[1] + " - A porta abriu!");
+                }
+                else if (array[2] == 0 && array[0] < 10)
+                {
+                    Console.WriteLine("0"+array[0] + ":" + array[1] + " - A porta fechou!");
+                }
+                else if (array[2] == 1 && array[0] < 10)
+                {
+                    Console.WriteLine("0"+array[0] + ":" + array[1] + " - A porta abriu!");
+                }
+                else if (array[2] == 0 && array[1] < 10)
+                {
+                    Console.WriteLine(array[0] + ":0" + array[1] + " - A porta fechou!");
+                }
+                else if (array[2] == 1 && array[1] < 10)
+                {
+                    Console.WriteLine( array[0] + ":0" + array[1] + " - A porta abriu!");
+                }
+                else if (array[2] == 0) {
+                    Console.WriteLine(array[0] + ":" + array[1] + " - A porta fechou!");
+                }
+                else if (array[2] == 1)
+                {
+                    Console.WriteLine(array[0] + ":" + array[1] + " - A porta abriu!");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
