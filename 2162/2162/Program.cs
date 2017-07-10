19b7e920b5235e7f741406af13ff66e7abcfb2c9
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-09 23:12:01 -0300

    Problema 2162-Picos e Vales-C#

---
+++ 2162/2162/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int[] vetor = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int i = 0;
+            if (vetor.Length == 2)
+            {
+                if (vetor[0] == vetor[1])
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(1);
+                }
+            }
+            else
+            {
+                for (i = 0; i < vetor.Length - 1; i++)
+                {
+                    if (vetor[i] == vetor[i + 1] || vetor[i] > vetor[i + 1] && vetor[i + 1] > vetor[i + 2] || vetor[i] < vetor[i + 1] && vetor[i + 1] < vetor[i + 2])
+                    {
+                        Console.WriteLine(0);
+                        break;
+                    }
+
+                }
+            }
+            if (i == vetor.Length - 1)
+            {
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
