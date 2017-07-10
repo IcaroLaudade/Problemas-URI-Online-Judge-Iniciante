699e6de28f8a07367909277619ee9253bcee275e
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-10 19:43:06 -0300

    Problema 1984-O Enigma do Pronalândia-C#

---
+++ 1984/1984/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1984
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] a = Console.ReadLine().ToCharArray();
+            Array.Reverse(a);
+            string b = new string(a);
+            Console.WriteLine(b);
+        }
+    }
+}
