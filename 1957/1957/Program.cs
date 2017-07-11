6d4e60126d1601005670cfeb23d61f92073b2ec2
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-11 18:18:33 -0300

    Problema 1957-Converter para Hexadecimal-C#

---
+++ 1957/1957/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1957
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hex = int.Parse(Console.ReadLine()).ToString("X");
+            Console.WriteLine(hex);
+        }
+    }
+}
