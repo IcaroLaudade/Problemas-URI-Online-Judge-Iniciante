4c61ddfe3e6ab6c3496df4dda64d1c933a257798
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-11 19:02:07 -0300

    Problema 2143-A Volta do Radar-C#

---
+++ 2143/2143/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2143
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a == 0)
+                {
+                    break;
+                }
+                
+                for (int i = 0; i < a; i++)
+                {
+                    int b = int.Parse(Console.ReadLine());
+                    if (b % 2 == 1)
+                    {
+                        Console.WriteLine(((b - 1) * 2) + 1);
+                    }
+                    else {
+                        Console.WriteLine((b - 1) * 2);
+                    }
+                }
+            }
+        }
+    }
+}
