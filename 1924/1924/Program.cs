2d277bb18e96fcb70113ce040af82f4049d99d6a
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-13 14:07:30 -0300

    Problema 1924-Vitória e a Indecisão-C#

---
+++ 1924/1924/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1924
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short i = short.Parse(Console.ReadLine());
+            string s;
+            for (int c = 0; c < i; c++)
+            {
+                s = Console.ReadLine();
+            }
+            Console.WriteLine("Ciencia da Computacao");
+        }
+    }
+}
