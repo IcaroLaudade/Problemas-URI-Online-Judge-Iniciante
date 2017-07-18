5cdd59122f0574ed1c5892c290923eeadc0fd103
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-18 14:48:45 -0300

    Problema 1933-Tri-du-C#

---
+++ 1933/1933/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1933
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            if (array[0] == array[1])
+            {
+                Console.WriteLine(array[0]);
+            }
+            else {
+                if (array[0] > array[1])
+                {
+                    Console.WriteLine(array[0]);
+                }
+                else {
+                    Console.WriteLine(array[1]);
+                }
+            }          
+        }
+    }
+}
