c253541be5c6caca2df9133ed66078e0f2eded82
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-18 14:16:00 -0300

    Problema 1930-Tomadas-C#

---
+++ 1930/1930/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1930
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            int soma = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                soma += array[i];
+            }
+            Console.WriteLine(soma - 3);
+        }
+    }
+}
