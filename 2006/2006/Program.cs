058c71184635172fd44b321642c3e4966345c710
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-07-18 14:02:05 -0300

    Problema 2006-Identificando o Chá-C#

---
+++ 2006/2006/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int cont = 0;
+            int[] numero=Array.ConvertAll(Console.ReadLine().Split(' '),int.Parse);
+            for (int i = 0; i < numero.Length; i++) { 
+            if(numero[i]==num){
+                cont++;
+            }
+            }
+            Console.WriteLine(cont);
+        }
+    }
+}
