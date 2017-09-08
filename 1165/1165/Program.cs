f72b5571695a154112d10b82544a4a0bd336dc82
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-07 22:46:41 -0300

    Problema 1165-Número Primo-C#

---
+++ 1165/1165/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroCasos = int.Parse(Console.ReadLine());
+            int contador = 0;
+            int quebra;
+            while (contador < numeroCasos)
+            {
+                quebra = 0;
+                int numero = int.Parse(Console.ReadLine());
+                if (numero == 2)
+                {
+                    Console.WriteLine("2 eh primo");
+                }
+                else
+                {
+                    for (int i = 2; i < numero; i++)
+                    {
+                        if (numero % i == 0)
+                        {
+                            quebra = 1;
+                        }
+                    }
+                    if (quebra == 0)
+                    {
+                        Console.WriteLine(numero + " eh primo");
+                    }
+                    else {
+                        Console.WriteLine(numero + " nao eh primo");
+                    }
+                }
+                contador++;
+            }
+        }
+    }
+}
