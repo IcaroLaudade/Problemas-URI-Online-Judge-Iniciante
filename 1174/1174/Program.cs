6dea399b10b7599b31284f4da6222c6817fa19c1
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-09 22:06:20 -0300

    Problema 1174-SELEÇÃO EM VETOR I-C#

---
+++ 1174/1174/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1174
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetor = new double[100];
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                vetor[i] = double.Parse(Console.ReadLine());
+                if (vetor[i] <= 10) {
+                    Console.WriteLine("A["+i+"] = {0}",vetor[i].ToString("#0.0").Replace(",","."));
+                }
+            }
+        }
+    }
+}
