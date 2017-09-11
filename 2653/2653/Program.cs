8c6a6003f0f5b80842c00012ef1bb64d40d072aa
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-10 23:45:09 -0300

    Problema 2653-DIJKSTRA-C#

---
+++ 2653/2653/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2653
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            List<string> lista = new List<string>();
+            while ((line = System.Console.ReadLine()) != null)
+            {
+                        if (!lista.Contains(line))
+                        {
+                            lista.Add(line);
+                        }
+            }
+            Console.WriteLine(lista.Count);
+        }
+    }
+}
