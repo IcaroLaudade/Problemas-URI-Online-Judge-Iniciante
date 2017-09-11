3ff6a5e2a43bb28829633203ba9d7d04add08d48
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-10 23:17:55 -0300

    Problema 1176-Fibonacci em Vetor-C#

---
+++ 1176/1176/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1176
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int casosTeste = int.Parse(Console.ReadLine());
+            for (int h = 0; h < casosTeste; h++) {
+                int numero = int.Parse(Console.ReadLine());
+                long soma = 0;
+                long i = 1, j = 0;
+                    for (int c = 0; c < numero; c++)
+                    {
+                        soma=i+j;
+                        i = j;
+                        j = soma;
+                    }
+                    Console.WriteLine("Fib(" + numero + ") = "+j);        
+            }
+        }
+    }
+}
