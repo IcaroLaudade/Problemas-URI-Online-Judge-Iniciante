c2aba74b738abec8eefdd03856c130b1ada380d7
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-11 18:43:00 -0300

    Problema 2650-Construindo Muralhas-C#

---
+++ 2650/2650/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2650
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numeros = Console.ReadLine().Split(' ');
+            int altura=int.Parse(numeros[1]);
+            for (int i = 0; i < int.Parse(numeros[0]); i++) {
+                string s = Console.ReadLine();
+                int idx = s.LastIndexOf(' ');
+                string nome =s.Substring(0, idx);
+                string alturaGigante=s.Substring(idx + 1);
+                if (int.Parse(alturaGigante) > altura) {
+                    Console.WriteLine(nome);
+                }
+            }
+        }
+    }
+}
