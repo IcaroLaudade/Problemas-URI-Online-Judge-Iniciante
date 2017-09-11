36172f294e4fe4266da834a7ba3b30315b670da0
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-11 14:35:53 -0300

    Problema 1177-PREENCHIMENTO DE VETOR II-C#

---
+++ 1177/1177/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1177
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int contador = 0;
+            for (int n = 0; n < 1000; n++) {
+                if (contador < numero)
+                {
+                    Console.WriteLine("N[" + n + "] = " + contador);
+                }
+                else {
+                    contador = 0;
+                    Console.WriteLine("N[" + n + "] = " + contador);
+                }
+                contador++;          
+            }
+        }
+    }
+}
