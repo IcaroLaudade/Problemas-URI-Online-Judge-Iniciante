f78a99a1af54cfea3777113f03b582e5de6f85a0
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-11 21:46:26 -0300

    Problema 2523-A Mensagem de Will-C#

---
+++ 2523/2523/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2523
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = System.Console.ReadLine()) != null)
+            {
+                char[] letras = line.ToCharArray();
+                string txt = "";
+                int numero = int.Parse(Console.ReadLine());
+                string[] numeros = Console.ReadLine().Split(' ');
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    txt += letras[int.Parse(numeros[i])-1];
+                }
+                Console.WriteLine(txt);
+            }
+
+        }
+    }
+}
