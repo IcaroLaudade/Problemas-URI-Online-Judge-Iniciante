70c247dfeaf502d76e618f80bce111f6b1519d06
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-11 22:04:30 -0300

    Problema 2581-I am Toorg!-C#

---
+++ 2581/2581/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2581
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            string txt;
+            for (int i = 0; i < numero; i++) {
+                txt = Console.ReadLine();
+                Console.WriteLine("I am Toorg!");
+            }
+        }
+    }
+}
