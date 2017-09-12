b46965241b64905dfd76ed0b2c26f03744caadb0
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-11 22:13:36 -0300

    Problema 2172-Evento-C#

---
+++ 2172/2172/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2172
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                string[] s = Console.ReadLine().Split(' ');
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+                if (a == 0 && b == 0) {
+                    break;
+                }
+                Console.WriteLine(a*b);
+            }
+        }
+    }
+}
