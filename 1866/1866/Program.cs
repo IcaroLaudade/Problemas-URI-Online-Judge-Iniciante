e88ca3aeefe536ca0c0c74dd643c6c8687583cf0
Icaro Alcantara Laudade <icaroalcantara11@hotmail.com>
2017-09-12 17:43:30 -0300

    Problema 1866-Conta-C#

---
+++ 1866/1866/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1866
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = int.Parse(Console.ReadLine());
+            for (int i = 0; i < c; i++)
+            {
+                int numero = int.Parse(Console.ReadLine()) % 2;
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
